2ad4e4ad77e0c50a9e95439091c691fe349fedbe
Francois Lerosier <francois.lerosier@gmail.com>
2024-05-21 10:07:33 +0200

    fist class created

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-console.log(c);
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Boundary {
+  constructor({ position }) {
+    this.position = position
+    this.width = 40
+    this.height = 40
+  }
+
+  draw() {
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+const boundaries = [
+  new Boundary({
+    position: {
+      x: 0,
+      y: 0
+    }
+  }),
+  new Boundary({
+    position: {
+      x: 41,
+      y: 0
+    }
+  })
+]
+
+boundaries.forEach((boundary) => {
+  boundary.draw()
+})
