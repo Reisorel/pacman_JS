413f76728e59afc3238f18378608b855e31dbd68
Francois Lerosier <francois.lerosier@gmail.com>
2024-05-21 11:01:16 +0200

    1/6

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Boundary {
+  static width = 40
+  static heigth = 40
   constructor({ position }) {
     this.position = position
     this.width = 40
@@ -17,21 +19,61 @@ class Boundary {
   }
 }
 
-const boundaries = [
-  new Boundary({
-    position: {
-      x: 0,
-      y: 0
-    }
-  }),
-  new Boundary({
-    position: {
-      x: 41,
-      y: 0
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position
+    this.velocity = velocity
+    this.radius = 15
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.position.x, this.position.y, this.radius, 0,
+      Math.PI * 2)
+    c.fillStyle = 'yellow',
+      c.fill()
+    c.closePath()
+  }
+}
+
+const map = [
+  ['-', '-', '-', '-', '-', '-',],
+  ['-', ' ', ' ', ' ', ' ', '-',],
+  ['-', ' ', '-', '-', ' ', '-',],
+  ['-', ' ', ' ', ' ', ' ', '-',],
+  ['-', '-', '-', '-', '-', '-',]
+]
+const boundaries = []
+
+const player = new Player({
+  position: {
+    x: Boundary.width + Boundary.width / 2,
+    y: Boundary.heigth + Boundary.heigth / 2,
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
+})
+
+map.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    switch (symbol) {
+      case '-':
+        boundaries.push(
+          new Boundary({
+            position: {
+              x: Boundary.width * j,
+              y: Boundary.heigth * i
+            }
+          }))
+        break
     }
   })
-]
+})
 
 boundaries.forEach((boundary) => {
   boundary.draw()
 })
+
+player.draw()
